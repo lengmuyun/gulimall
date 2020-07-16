1abda70be235bf232feb40e16454730d8758a5d5
lengmuyun <971434648@qq.com>
2020-07-16 15:18:11 +0800

    账号安全

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/AttrGroupServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/AttrGroupServiceImpl.java
@@ -2,15 +2,16 @@ package com.atguigu.gulimall.product.service.impl;
 
 import com.atguigu.common.utils.PageUtils;
 import com.atguigu.common.utils.Query;
-import org.springframework.stereotype.Service;
-import java.util.Map;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-
 import com.atguigu.gulimall.product.dao.AttrGroupDao;
 import com.atguigu.gulimall.product.entity.AttrGroupEntity;
 import com.atguigu.gulimall.product.service.AttrGroupService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.Map;
 
 
 @Service("attrGroupService")
@@ -18,12 +19,31 @@ public class AttrGroupServiceImpl extends ServiceImpl<AttrGroupDao, AttrGroupEnt
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        return getPageUtils(params, new QueryWrapper<>());
+    }
+
+    private PageUtils getPageUtils(Map<String, Object> params, QueryWrapper<AttrGroupEntity> queryWrapper) {
         IPage<AttrGroupEntity> page = this.page(
                 new Query<AttrGroupEntity>().getPage(params),
-                new QueryWrapper<AttrGroupEntity>()
+                queryWrapper
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public PageUtils queryPage(Map<String, Object> params, Long catelogId) {
+        QueryWrapper<AttrGroupEntity> queryWrapper;
+        if (catelogId == 0) {
+            queryWrapper = new QueryWrapper<>();
+        } else {
+            String key = (String) params.get("key");
+            queryWrapper = new QueryWrapper<AttrGroupEntity>().eq("catelog_id", catelogId);
+            if (!StringUtils.isEmpty(key)) {
+                queryWrapper.and(wrapper -> wrapper.eq("attr_group_id", key).or().like("attr_group_name", key));
+            }
+        }
+        return getPageUtils(params, queryWrapper);
     }
 
 }
